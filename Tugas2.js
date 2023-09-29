d548880b207b8ae30a563ffe6b957d0746396249
Suganda <129074317+suganda02@users.noreply.github.com>
2023-09-29 16:35:51 +0700

    Add files via upload

---
+++ Tugas2.js
@@ -0,0 +1,11 @@
+let NAMA = "SUGANDA"
+let NIM = "20220040258"
+
+console.log("NAMA: + NAMA");
+console.log("NIM: + NIM") ;
+
+let angka1 = 5;
+let angka2 = 9;
+
+let jumlah = angka1 + angka2;
+console.log("hasil penjumlahan: " + jumlah);
